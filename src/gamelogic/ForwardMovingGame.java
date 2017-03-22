f1f2f104825eb385003acffb9e2decbbab9eb4f8
benjaminwonglee <benjaminwonglee@gmail.com>
2017-03-22 13:46:25 +1300

    Filled GameOverScreen code. Still under construction.
    Code currently testing Game over screen

--- src/gamelogic/ForwardMovingGame.java
+++ src/gamelogic/ForwardMovingGame.java
@@ -4,10 +4,13 @@ import graphics.Frame;
 
 public class ForwardMovingGame {
 	public ForwardMovingGame(){
-		Logic l = new Logic();
-		Frame f = new Frame(l);
-		ClockRunner clock = new ClockRunner(f);
-		f.setClock(clock);
+		//TODO: TEST gameover screen
+		new GameOverScreen();
+		
+//		Logic l = new Logic();
+//		Frame f = new Frame(l);
+//		ClockRunner clock = new ClockRunner(f);
+//		f.setClock(clock);
 	}
 
 	public static void main(String args[]) {
--- src/gamelogic/GameOverScreen.java
+++ src/gamelogic/GameOverScreen.java
@@ -1,5 +1,45 @@
 package gamelogic;
 
-public class GameOverScreen {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameOverScreen extends JFrame {
+	private static final long serialVersionUID = -4909362608024151100L;
+
+	public GameOverScreen() {
+		setPreferredSize(new Dimension(800, 800));
+		this.setTitle("Game Over");
+		GameOverPanel panel = new GameOverPanel();
+		this.add(panel);
+		this.setResizable(false);
+		this.pack();
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+
+	private class GameOverPanel extends JPanel {
+		private static final long serialVersionUID = -8583261398361793225L;
+
+		@Override
+		public void paintComponent(Graphics g) {
+			int inward = 0;
+			for (int i = 0; i < 7; i++) {
+				if (inward % 2 == 1) {
+					g.setColor(new Color(0, 0, 100));
+				} else {
+					g.setColor(new Color(0, 0, 50));
+				}
+				g.fillRect(0 + inward, 0 + inward, this.getWidth() - (inward * 2), this.getHeight() - (inward * 2));
+				inward += 41;
+			}
+			//TODO: Draw random red triangles here
+			g.setColor(new Color(150, 0, 0));
+			//g.fillPolygon(xPoints, yPoints, nPoints);
+			
+		}
+	}
 }

ee4217b6a1ab1fa4466bbf7ffa2f02a291f9e3a4
benjaminwonglee <benjaminwonglee@gmail.com>
2017-03-22 13:48:26 +1300

    GameOverScreen Testing moved to different branch

--- src/gamelogic/ForwardMovingGame.java
+++ src/gamelogic/ForwardMovingGame.java
@@ -5,12 +5,12 @@ import graphics.Frame;
 public class ForwardMovingGame {
 	public ForwardMovingGame(){
 		//TODO: TEST gameover screen
-		new GameOverScreen();
+		//new GameOverScreen();
 		
-//		Logic l = new Logic();
-//		Frame f = new Frame(l);
-//		ClockRunner clock = new ClockRunner(f);
-//		f.setClock(clock);
+		Logic l = new Logic();
+		Frame f = new Frame(l);
+		ClockRunner clock = new ClockRunner(f);
+		f.setClock(clock);
 	}
 
 	public static void main(String args[]) {
